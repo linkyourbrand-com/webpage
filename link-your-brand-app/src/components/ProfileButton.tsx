0876628bd52f2f63a5e2383c0a1aec61cca09aa1
Nikolas Hunter <166237108+NikolasHunter515@users.noreply.github.com>
2025-10-08 01:07:46 +0000

    client issue fixed, component still needs work

--- link-your-brand-app/src/components/ProfileButton.tsx
+++ link-your-brand-app/src/components/ProfileButton.tsx
@@ -1,17 +1,18 @@
-//import React, { useState } from 'react'
+'use client';
+import React, { useState } from 'react';
+
 interface ProfileButtonsProps {
     username?: string // optional string
     loggedIn: boolean // must be a boolean
     imageUrl?: string // optional string
 }
 function ProfileButton({ username, loggedIn, imageUrl }: ProfileButtonsProps) {
-    //const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false)
     // 🔁 toggles the open/closed state
-    //const toggleDropdown = () => setOpen(!open)
-    //onClick={toggleDropdown}
+    const toggleDropdown = () => setOpen(!open)
     return (
         <div>
-            <button >Menu</button>
+            <button onClick={toggleDropdown}>Menu</button>
             <img src={imageUrl || 'profile image.png'} alt="Profile"
             width={50}
             height={50}/>
