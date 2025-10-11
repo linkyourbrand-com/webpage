26d65c7727300ff515138358533bdbc9adbe8a0a
Gustensen <dabresilclark@gmail.com>
2025-10-11 18:24:07 -0400

    chore: add csstype package and update package dependencies
    - Added csstype package (version 3.1.3) with TypeScript and Flow types for CSS.
    - Updated package-lock.json to include csstype and its dependencies.
    - Updated package.json to include devDependencies for @types/react and @types/react-dom.

--- link-your-brand-app/src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-import ProfileButton from '@/components/ProfileButton';
-const page = () => {return <ProfileButton username='james' loggedIn></ProfileButton>}
-export default page;
---
+++ linkYourBrandApp/link your brand web/src/components/ProfileButton.tsx
@@ -0,0 +1,6 @@
+export default function ProfileButton() {
+  return (
+    <> <img src="linkYourBrandApp\link your brand web\src\assets\profile image.png" alt="Profile" />
+      </>
+  );
+}
