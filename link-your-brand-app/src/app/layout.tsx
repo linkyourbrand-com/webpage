736239ae5a2f5dbce4c8fe363249d22ae5ecc5a6
Nikolas Hunter <166237108+NikolasHunter515@users.noreply.github.com>
2025-10-15 21:35:20 +0000

    Bootstrap really added to project

--- link-your-brand-app/src/app/layout.tsx
+++ link-your-brand-app/src/app/layout.tsx
@@ -28,6 +28,8 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/svg+xml" href="/vite.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         <title>link-your-brand-web</title>
       </head>
       <body>
