fad201b1572dcb8fc23bdeb3cdb167605c156681
Nikolas Hunter <166237108+NikolasHunter515@users.noreply.github.com>
2025-11-28 20:41:30 +0000

    More changes made to scripts

--- link-your-brand-app/src/app/api/cognito/scripts/test-cognito.sh
+++ link-your-brand-app/src/app/api/cognito/scripts/test-cognito.sh
@@ -3,14 +3,20 @@
 # Test sign up
 echo "Testing sign up..."
 #http://localhost:4566/_localstack/health
-#change port to 3000
+#actually create the account
 #curl -X POST http://localhost:3000/api/auth/signup \
 #  -H "Content-Type: application/json" \
 #  -d '{"email":"test@example.com","password":"Test1234"}'
 
+awslocal cognito-idp sign-up \
+  --client-id b7da0nu45bjlnsy7vetusr15vf \
+  --username test@example.com \
+  --password "Password123!"
+
+
 #verify account manuelly
 vrfy=$(awslocal cognito-idp admin-confirm-sign-up \
-    --user-pool-id us-east-1_d81b57b5bfc6437b8fd220366a2ed969 \
+    --user-pool-id us-east-1_9009eea040bb404d8054087e3498fde2 \
     --username test@example.com)
 echo "$vrfy"
 
@@ -18,4 +24,4 @@ echo "\n\nTesting sign in..."
 # Test sign in
 curl -X POST http://localhost:3000/api/auth/signin \
   -H "Content-Type: application/json" \
-  -d '{"email":"test@example.com","password":"Test1234"}'
+  -d '{"email":"test@example.com","password":"Password123!"}'
